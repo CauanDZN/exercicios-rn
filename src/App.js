8474acff1dc53f693985ef3c0693832417504095
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-02 18:09:28 -0300

    feat: ecmascript module system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { SafeAreaView, Text } from "react-native"
+import { Comp1, Comp2 } from "./components/Multi"
 import Primeiro from "./components/Primeiro"
 
 function App() {
     return (
         <SafeAreaView>
             <Primeiro />
+            <Comp1 />
+            <Comp2 />
         </SafeAreaView>
     )
 }
---
+++ src/components/Multi.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text } from "react-native";
+
+export function Comp1(){
+    return(
+        <Text>Componente 1</Text>
+    )
+}
+
+export function Comp2(){
+    return(
+        <Text>Componente 2</Text>
+    )
+}
+
+export function Comp3(){
+    return(
+        <Text>Componente 3</Text>
+    )
+}
