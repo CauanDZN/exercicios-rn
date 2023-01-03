e1d12a439757d510acc27ba12a5b9bd4964a3e02
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-03 02:03:47 -0300

    feat: direct communication

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Aleatorio from "./components/Aleatorio";
 import Titulo from "./components/Titulo";
 import Botao from "./components/Botao";
 import Contador from "./components/Contador";
+import Pai from "./components/direta/Pai";
 
 function App() {
     return (
@@ -18,6 +19,7 @@ function App() {
             <CompPadrao />
             <Comp1 />
             <Comp2 />
+            <Pai />
             <Contador inicial={0} passo={1} />
             <Botao />
         </SafeAreaView>
