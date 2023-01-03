beb6422fbc22ca4f726cc9c2fee5ee32275cfb12
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-03 01:50:45 -0300

    feat: usestate

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MinMax from "./components/MinMax";
 import Aleatorio from "./components/Aleatorio";
 import Titulo from "./components/Titulo";
 import Botao from "./components/Botao";
+import Contador from "./components/Contador";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
             <CompPadrao />
             <Comp1 />
             <Comp2 />
+            <Contador inicial={0} passo={1} />
             <Botao />
         </SafeAreaView>
     )
