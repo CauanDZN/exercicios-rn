7d1725844e7b2c512d9e312351919e30ae6add52
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-03 16:41:32 -0300

    feat: indirect communication

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
-import CompPadrao, {Comp1, Comp2} from "./components/Multi";
-import Primeiro from "./components/Primeiro";
-import MinMax from "./components/MinMax";
-import Aleatorio from "./components/Aleatorio";
-import Titulo from "./components/Titulo";
-import Botao from "./components/Botao";
-import Contador from "./components/Contador";
-import Pai from "./components/direta/Pai";
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
 
-function App() {
-    return (
-        <SafeAreaView style={style.app}>
-            <Titulo principal="Cadastro" secundario="Tela de Cadastro" />
-            <MinMax min={3} max={20} />
-            <Aleatorio min={3} max={20} />
-            <Primeiro />
-            <CompPadrao />
-            <Comp1 />
-            <Comp2 />
-            <Pai />
-            <Contador inicial={0} passo={1} />
-            <Botao />
-        </SafeAreaView>
-    )
-}
+import Contador from './components/contador/Contador'
+// import Pai from './components/indireta/Pai'
+// import Pai from './components/direta/Pai'
+// import Contador from './components/Contador'
+// import Botao from './components/Botao'
+// import Titulo from './components/Titulo'
+// import Aleatorio from './components/Aleatorio'
+// import MinMax from './components/MinMax'
+// import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+// import Primeiro from './components/Primeiro'
+
+
+export default () => (
+    <SafeAreaView style={style.App}>
+        <Contador />
+        {/*
+        <Pai />
+        <Pai />
+        <Contador inicial={100} passo={13} />
+        <Contador />
+        <Botao />
+        <Titulo principal="Cadastro Produto"
+            secundario="Tela de Cadastro do Produto" />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <MinMax min={3} max={20} />
+        <MinMax min={1} max={94} />
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro /> */}
+    </SafeAreaView>
+)
 
 const style = StyleSheet.create({
-    app: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    App: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
         padding: 20
     },
-})
 
-export default App
+})
